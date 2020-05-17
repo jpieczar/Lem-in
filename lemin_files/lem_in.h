66498b596cfda17238fcb9b29be313e5b71c13cb
jpieczar <jpieczar@student.wethinkcode.co.za>
2020-05-17 19:39:46 +0200

    Lem-in done

---
+++ lemin_files/lem_in.h
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpieczar <jpieczar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/10 10:40:17 by jpieczar          #+#    #+#             */
+/*   Updated: 2019/09/16 09:18:51 by jpieczar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+# include "../libft/libft.h"
+
+typedef struct			s_path
+{
+	t_list				*aroute;
+	t_list				*broute;
+	int					size;
+	struct s_path		*next;
+	struct s_path		*prev;
+}						t_path;
+
+typedef struct			s_room
+{
+	t_list				*links;
+	char				*name;
+	int					ants;
+	int					s;
+	int					e;
+	int					used;
+	struct s_room		*next;
+}						t_room;
+
+void					makeroom(t_room **head, char *name, t_list *links);
+int						checkroom(t_room *head, char *name);
+t_room					*newroom(char *name, t_list *links);
+void					deletemap(t_room **head);
+void					endorstart(t_room *head, char *name, char *c);
+
+t_room					*findinmap(t_room **head, char *name);
+t_room					**linkrooms(t_room **head, char **arr);
+
+void					pushpath(t_path **head, t_path *new_r);
+t_path					*duppath(t_path *src);
+t_path					*newpath(t_list *list);
+void					deletepath(t_path **ptr);
+void					deletepaths(t_path **head);
+
+int						checkpaths(t_path *head, t_room *room);
+t_path					*pathfinder(t_room *rooms);
+void					pathstepper(t_path **head);
+void					addstep(t_path **head, t_path *cur,
+							t_room *room);
+
+t_room					**makelinks(t_room **head, char *s);
+t_room					*make(t_room **head);
+t_list					**makearr(int n);
+
+void					linkerror(void);
+void					starterror(void);
+void					roomerror(void);
+void					anterror(void);
+void					outputant(int ant, char *name);
+void					deletelist(void *c);
+void					cleanpath(t_path *head);
+void					usedornot(t_path *r);
+void					makeants(t_room *head, char *ants);
+t_list					**pushant(t_list **arr, int n, t_path *paths);
+t_list					**addant(t_list **arr, int *loop,
+							int n, t_path *ref);
+void					outputants(t_path *path);
+int						possiblemove(t_path *path, int ants);
+
+void					checkrooms(t_room *head);
+void					checkforend(t_path *paths);
+
+#endif
